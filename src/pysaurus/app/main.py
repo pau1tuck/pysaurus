9276ae22f4f6437bdf7335cd9573b9ba371091ae
Paul Tuck <pau1tuck@outlook.com>
2022-12-25 17:23:07 +0700

    "2022-12-25_17:23_UTC+07_Sun"

--- src/pysaurus/app/main.py
+++ src/pysaurus/app/main.py
@@ -4,12 +4,24 @@ from data.wordlist_0_1_0 import words, length
 def wordcount():
     return length
 
+# First check if the search term exists in the database
 def check_for_word(word):
     if word not in words:
         return "Not found"
     else:
         retrieve_data(word)
 
+# If the search term exists return the full thesaurus payload
 def retrieve_data(word):
     json_data = []
     thesaurus_data = []
+    first_letter = word[0]
+
+    data = open("../data/thesauri/0_1_0/" + first_letter + ".json", "r")
+    json_data = json.load(data)
+
+    for entry in json_data:
+        if entry["term"] == word:
+            thesaurus_data = entry["data"]
+
+    return thesaurus_data
