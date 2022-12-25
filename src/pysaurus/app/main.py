fd60467548f230ed79dd5ceb0a0617a94f8938ee
Paul Tuck <pau1tuck@outlook.com>
2022-12-25 19:21:30 +0700

    "2022-12-25_19:21_UTC+07_Sun"

--- src/pysaurus/app/main.py
+++ src/pysaurus/app/main.py
@@ -4,32 +4,41 @@ from data.wordlist_0_1_0 import words, length
 def wordcount():
     return length
 
-# First check if the search term exists in the database:
-def check_for_word(word):
+# If the search term exists, return the full thesaurus payload:
+def retrieve_data(word):
     if word not in words:
         return False
     else:
-        retrieve_data(word)
+        json_data = []
+        thesaurus_data = []
+        first_letter = word[0]
 
-# If the search term exists, return the full thesaurus payload:
-def retrieve_data(word):
-    json_data = []
-    thesaurus_data = []
-    first_letter = word[0]
+        data = open("../data/thesauri/0_1_0/" + first_letter + ".json", "r")
+        json_data = json.load(data)
 
-    data = open("../data/thesauri/0_1_0/" + first_letter + ".json", "r")
-    json_data = json.load(data)
+        for entry in json_data:
+            if entry["term"] == word:
+                thesaurus_data = entry["data"]
 
-    for entry in json_data:
-        if entry["term"] == word:
-            thesaurus_data = entry["data"]
-
-    return thesaurus_data
+        return thesaurus_data
 
 def get_synonyms(word):
-    thesaurus_data = check_for_word(word)
+    thesaurus_data = retrieve_data(word)
     if thesaurus_data == False:
         return False
     else:
+        data = []
         for entry in thesaurus_data:
-            definition = {entry["definition"]: []}
+            definition = entry["definition"]
+            pos = entry["pos"]
+            synonyms = []
+            for synonym in entry["synonyms"]:
+                synonyms.append(synonym)
+            result = {
+                "term": word,
+                "definition": definition,
+                "class": pos,
+                "synonyms": synonyms
+            }
+            data.append(result)
+    return data
